27630023d2f05753fd432309b82be54a749647f6
Amir Shakibafar <amir.shakibafar82@gmail.com>
2025-05-12 01:49:33 +0330

    added pagination component basic structure

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import TableCoin from "../modules/TableCoin";
 import { getApiKey, getCointList } from "../../services/cryptoAPI";
-
+import Pagination from "../modules/Pagination"
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setisLoading] = useState(true);
-
 
   useEffect(() => {
     const options = {
@@ -24,14 +23,19 @@ function HomePage() {
     axios
       .request(options)
       .then((response) => {
-        setCoins(response.data)
+        setCoins(response.data);
         setisLoading(false);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
-  return <TableCoin coins={coins} isLoading={isLoading}/>;
+  return (
+    <div>
+      <Pagination/>
+      <TableCoin coins={coins} isLoading={isLoading} />
+    </div>
+  );
 }
 
 export default HomePage;
